ce3244d651bac3b8fc1cdb3e22abb7e463ff21e9
DentistOmarAshraf <omar_ashraf@live.com>
2024-11-10 12:58:07 +0200

    some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,20 @@ import Header from "./component/Header";
 import Slogan from "./component/Slogan";
 import ProductCard from "./component/ProductCard";
 import ProductInfo from "./component/ProductInfo";
+import CategoryCard from "./component/CategoryCard";
+import AddMerchant from "./component/AddMerchant";
+import SignIn from "./component/SignIn";
 
 function App() {
   return (
     <>
       <Header />
       <Slogan text="Your Marketplace, Anytime, Anywhere" />
-      <ProductCard />
+      {/* <ProductCard /> */}
       <ProductInfo />
+      {/* <CategoryCard /> */}
+      {/* <AddMerchant /> */}
+      {/* <SignIn /> */}
     </>
   );
 }
